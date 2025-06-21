e6b0a065c5e99a9a41aa9c0d610acb976c28bf09
Nils Osswald <contact@nilsosswald.de>
2025-06-21 20:31:58 +0200

    remove show ms option for now

--- core/src/main/java/pw/rebux/parkourdisplay/core/widget/LastTimingWidget.java
+++ core/src/main/java/pw/rebux/parkourdisplay/core/widget/LastTimingWidget.java
@@ -2,30 +2,25 @@ package pw.rebux.parkourdisplay.core.widget;
 
 import static net.labymod.api.client.component.Component.translatable;
 
-import lombok.Getter;
-import lombok.experimental.Accessors;
 import net.labymod.api.client.gui.hud.hudwidget.text.TextHudWidget;
 import net.labymod.api.client.gui.hud.hudwidget.text.TextHudWidgetConfig;
 import net.labymod.api.client.gui.hud.hudwidget.text.TextLine;
-import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
-import net.labymod.api.configuration.loader.property.ConfigProperty;
 import pw.rebux.parkourdisplay.core.ParkourDisplayAddon;
-import pw.rebux.parkourdisplay.core.widget.LastTimingWidget.LastTimingWidgetConfig;
 
-public class LastTimingWidget extends TextHudWidget<LastTimingWidgetConfig> {
+public class LastTimingWidget extends TextHudWidget<TextHudWidgetConfig> {
 
   private final ParkourDisplayAddon addon;
 
   private TextLine textLine;
 
   public LastTimingWidget(ParkourDisplayAddon addon) {
-    super("last_timing", LastTimingWidgetConfig.class);
+    super("last_timing");
     this.bindCategory(addon.category());
     this.addon = addon;
   }
 
   @Override
-  public void load(LastTimingWidgetConfig config) {
+  public void load(TextHudWidgetConfig config) {
     super.load(config);
 
     this.textLine = createLine(translatable("parkourdisplay.labels.last_timing"), "-");
@@ -36,14 +31,5 @@ public class LastTimingWidget extends TextHudWidget<LastTimingWidgetConfig> {
     var lastTiming = this.addon.playerParkourState().lastTiming();
 
     this.textLine.updateAndFlush(lastTiming);
-  }
-
-  @Accessors(fluent = true)
-  @Getter
-  public static class LastTimingWidgetConfig extends TextHudWidgetConfig {
-
-    // TODO: Implement
-    @SwitchSetting
-    private final ConfigProperty<Boolean> showMillis = new ConfigProperty<>(true);
   }
 }
