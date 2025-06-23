c35080ac94969814dbac11556da13d75b760171c
Nils Osswald <contact@nilsosswald.de>
2025-06-23 09:31:29 +0200

    fix: clone block state position reference

--- core/src/main/java/pw/rebux/parkourdisplay/core/LandingBlockManager.java
+++ core/src/main/java/pw/rebux/parkourdisplay/core/LandingBlockManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import lombok.Getter;
 import net.labymod.api.client.world.block.BlockState;
 import net.labymod.api.util.math.position.Position;
+import net.labymod.api.util.math.vector.IntVector3;
 import pw.rebux.parkourdisplay.core.state.TickPosition;
 
 @Getter
@@ -12,7 +13,12 @@ public class LandingBlockManager {
   private final ArrayList<LandingBlock> landingBlocks = new ArrayList<>();
 
   public void register(BlockState blockState) {
-    this.landingBlocks.add(new LandingBlock(blockState.position(), blockState.bounds()));
+    var pos = blockState.position();
+
+    this.landingBlocks.add(
+        new LandingBlock(
+            new IntVector3(pos.getX(), pos.getY(), pos.getZ()),
+            blockState.bounds()));
   }
 
   public void checkOffsets(Position position, TickPosition lastTick, TickPosition secondLastTick) {
