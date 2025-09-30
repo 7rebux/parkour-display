82fa71331dbf38487f7b92e88d692ab6fbbcc9d9
Nils Osswald <contact@nilsosswald.de>
2025-09-30 16:20:48 +0200

    Migrate to laby3d based rendering

---
+++ core/src/main/java/pw/rebux/parkourdisplay/core/util/RenderPrograms.java
@@ -0,0 +1,38 @@
+package pw.rebux.parkourdisplay.core.util;
+
+import net.labymod.api.laby3d.pipeline.RenderStates;
+import net.labymod.api.laby3d.vertex.VertexDescriptions;
+import net.labymod.laby3d.api.pipeline.ComparisonStrategy;
+import net.labymod.laby3d.api.pipeline.DrawingMode;
+import net.labymod.laby3d.api.pipeline.RenderState;
+import net.labymod.laby3d.api.pipeline.blend.DefaultBlendFunctions;
+import net.labymod.laby3d.api.pipeline.shader.ShaderProgramDescription;
+import net.labymod.laby3d.api.resource.AssetId;
+
+public final class RenderPrograms {
+
+  public static final RenderState DEBUG_BOUNDING_BOX = RenderState.builder()
+      .setId(buildStateId("quads"))
+      .setVertexDescription(VertexDescriptions.POSITION_UV_COLOR)
+      .setDrawingMode(DrawingMode.QUADS)
+      .setBlendFunction(DefaultBlendFunctions.TRANSLUCENT)
+      .setCull(false)
+      .setDepthTestStrategy(ComparisonStrategy.ALWAYS)
+      .setWriteDepth(false)
+      .setShaderProgramDescription(
+          ShaderProgramDescription.builder(RenderStates.DEFAULT_SHADER_SNIPPET)
+              .setId(buildProgramId("quads"))
+              .setVertexShader(RenderStates.SHADER_RESOLVER.apply("core/position_color.vsh"))
+              .setFragmentShader(RenderStates.SHADER_RESOLVER.apply("core/position_color.fsh"))
+              .build()
+      )
+      .build();
+
+  private static AssetId buildStateId(String name) {
+    return AssetId.of("parkourdisplay", "renderstate/" + name);
+  }
+
+  private static AssetId buildProgramId(String name) {
+    return AssetId.of("parkourdisplay", "shaderprogram/" + name);
+  }
+}
--- core/src/main/java/pw/rebux/parkourdisplay/core/util/RenderUtil.java
+++ core/src/main/java/pw/rebux/parkourdisplay/core/util/RenderUtil.java
@@ -1,16 +1,16 @@
 package pw.rebux.parkourdisplay.core.util;
 
 import net.labymod.api.Laby;
-import net.labymod.api.client.render.draw.RectangleRenderer;
+import net.labymod.api.client.render.batch.RectangleRenderContext;
 import net.labymod.api.client.render.matrix.Stack;
-import net.labymod.api.util.bounds.Rectangle;
 import net.labymod.api.util.math.AxisAlignedBoundingBox;
 import net.labymod.api.util.math.vector.DoubleVector3;
 import net.labymod.api.util.math.vector.IntVector3;
 
 public final class RenderUtil {
 
-  private static final RectangleRenderer rectangleRenderer = Laby.references().renderPipeline().rectangleRenderer();
+  private static final RectangleRenderContext RECTANGLE_RENDER_CONTEXT =
+      Laby.references().rectangleRenderContext();
 
   public static void renderBoundingBox(
       IntVector3 objectPosition,
@@ -36,46 +36,58 @@ public final class RenderUtil {
     stack.push();
     stack.translate(minX, minY, minZ);
     stack.rotate(90f, 1f, 0f, 0f);
-    rectangleRenderer.pos(0, 0, width, depth).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, width - outlineThickness, depth - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, width, depth, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, width - outlineThickness, depth - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
 
     // Top
     stack.push();
     stack.translate(minX, maxY, minZ);
     stack.rotate(90f, 1f, 0f, 0f);
-    rectangleRenderer.pos(0, 0, width, depth).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, width - outlineThickness, depth - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, width, depth, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, width - outlineThickness, depth - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
 
     // Left
     stack.push();
     stack.translate(minX, minY, minZ);
     stack.rotate(-90f, 0f, 1f, 0f);
-    rectangleRenderer.pos(0, 0, depth, height).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, depth - outlineThickness, height - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, depth, height, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, depth - outlineThickness, height - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
 
     // Right
     stack.push();
     stack.translate(maxX, minY, minZ);
     stack.rotate(-90f, 0f, 1f, 0f);
-    rectangleRenderer.pos(0, 0, depth, height).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, depth - outlineThickness, height - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, depth, height, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, depth - outlineThickness, height - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
 
     // Front
     stack.push();
     stack.translate(minX, minY, minZ);
-    rectangleRenderer.pos(0, 0, width, height).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, width - outlineThickness, height - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, width, height, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, width - outlineThickness, height - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
 
     // Back
     stack.push();
     stack.translate(minX, minY, maxZ);
-    rectangleRenderer.pos(0, 0, width, height).color(fillColor).render(stack);
-    rectangleRenderer.renderOutline(stack, Rectangle.absolute(0, 0, width - outlineThickness, height - outlineThickness), outlineColor, outlineThickness);
+    RECTANGLE_RENDER_CONTEXT.begin(stack);
+    RECTANGLE_RENDER_CONTEXT.render(0, 0, width, height, fillColor);
+    RECTANGLE_RENDER_CONTEXT.renderOutline(0, 0, width - outlineThickness, height - outlineThickness, outlineThickness, outlineColor, outlineColor);
+    RECTANGLE_RENDER_CONTEXT.uploadToBuffer(RenderPrograms.DEBUG_BOUNDING_BOX);
     stack.pop();
   }
 }
