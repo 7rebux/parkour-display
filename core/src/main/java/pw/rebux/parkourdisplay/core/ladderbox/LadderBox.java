6fa91d9ec38189109e33fc95e892d9951d037629
Nils Osswald <contact@nilsosswald.de>
2026-03-04 19:55:13 +0100

    Fix ladder intersection box expansion for all directions

--- core/src/main/java/pw/rebux/parkourdisplay/core/ladderbox/LadderBox.java
+++ core/src/main/java/pw/rebux/parkourdisplay/core/ladderbox/LadderBox.java
@@ -12,22 +12,31 @@ public class LadderBox {
   private final AxisAlignedBoundingBox boundingBox;
 
   @Getter(lazy = true)
-  private final AxisAlignedBoundingBox intersectionBox =
-      expand(this.boundingBox);
+  private final AxisAlignedBoundingBox intersectionBox = expand(this.boundingBox);
 
-  private AxisAlignedBoundingBox expand(AxisAlignedBoundingBox boundingBox) {
-    if (boundingBox.getXWidth() == 1.0D) {
-      return boundingBox
-          .minX(boundingBox.getMinX() + 0.3)
-          .maxX(boundingBox.getMaxX() - 0.3)
-          .minZ(boundingBox.getMaxZ())
-          .maxZ(boundingBox.getMinZ() + 0.7);
+  /**
+   * Expands the ladder bounding box outward into the climbable intersection region.
+   * The box extends 0.7 outward from the climbable face in the correct direction
+   * for both positive and negative axes.
+   */
+  private AxisAlignedBoundingBox expand(AxisAlignedBoundingBox box) {
+    var narrowIsX = box.getXWidth() <= box.getZWidth();
+    var narrowMin = narrowIsX ? box.getMinX() : box.getMinZ();
+    var narrowMax = narrowIsX ? box.getMaxX() : box.getMaxZ();
+    var wideMin = narrowIsX ? box.getMinZ() : box.getMinX();
+    var wideMax = narrowIsX ? box.getMaxZ() : box.getMaxX();
+
+    // If the narrow range is in the lower part of the block, face is at max; else at min
+    var faceAtMax = (narrowMin - Math.floor(narrowMin)) < 0.5;
+    var newNarrowMin = faceAtMax ? narrowMax : narrowMax - 0.7;
+    var newNarrowMax = faceAtMax ? narrowMin + 0.7 : narrowMin;
+    var newWideMin = wideMin + 0.3;
+    var newWideMax = wideMax - 0.3;
+
+    if (narrowIsX) {
+      return box.minX(newNarrowMin).maxX(newNarrowMax).minZ(newWideMin).maxZ(newWideMax);
     } else {
-      return boundingBox
-          .minZ(boundingBox.getMinZ() + 0.3)
-          .maxZ(boundingBox.getMaxZ() - 0.3)
-          .minX(boundingBox.getMaxX())
-          .maxX(boundingBox.getMinX() + 0.7);
+      return box.minZ(newNarrowMin).maxZ(newNarrowMax).minX(newWideMin).maxX(newWideMax);
     }
   }
 }
